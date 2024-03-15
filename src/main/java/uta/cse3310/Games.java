f1e7a4d6db81a47b6f07a9c14a7dd394d52bfb7f
Kaitlin Martin <kxm1944@mavs.uta.edu>
2024-03-15 17:54:05 -0500

    variables

--- src/main/java/uta/cse3310/Games.java
+++ src/main/java/uta/cse3310/Games.java
@@ -2,11 +2,18 @@
 //Group 6
 
 //not compiled yet
+/*notes:
+ * created this new class to eventually replace Game.
+ * I'm keeping them separate for now for organization purposes,
+ * and so that I can reference the Game class while building
+ */
 
 package uta.cse3310;
 
 public class Games {
 
-    
+    PlayerType Players;
+    public PlayerType[] Button;
+    public int GameId;
 
 }

ddcbd17e16ed7721fab2422a5db9524beb9b84ef
Diya <Diyaannshibu5@gmail.com>
2024-03-17 00:01:50 -0500

    Updated Word class

--- src/main/java/uta/cse3310/Word.java
+++ src/main/java/uta/cse3310/Word.java
@@ -1,5 +1,41 @@
 package uta.cse3310;
 
 public class Word {
-    
+    private int Xval;
+    private int Yval;
+    private WordType type;
+
+    // Constructor
+    public Word(int Xval, int Yval, WordType type) {
+        this.Xval = Xval;
+        this.Yval = Yval;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public int getXval() {
+        return Xval;
+    }
+
+    public void setXval(int Xval) {
+        this.Xval = Xval;
+    }
+
+    public int getYval() {
+        return Yval;
+    }
+
+    public void setYval(int Yval) {
+        this.Yval = Yval;
+    }
+
+    public WordType getType() {
+        return type;
+    }
+
+    public void setType(WordType type) {
+        this.type = type;
+    }
+
+    // Might include additional methods as necessary
 }

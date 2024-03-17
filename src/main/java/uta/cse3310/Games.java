690bcc249796b23c8c920306794896c2074db128
Kaitlin Martin <kxm1944@mavs.uta.edu>
2024-03-16 21:11:57 -0500

    added Leaderboard and Games

--- src/main/java/uta/cse3310/Games.java
+++ src/main/java/uta/cse3310/Games.java
@@ -1,19 +1,50 @@
 //Kaitlin Martin
 //Group 6
 
-//not compiled yet
 /*notes:
  * created this new class to eventually replace Game.
  * I'm keeping them separate for now for organization purposes,
  * and so that I can reference the Game class while building
  */
 
-package uta.cse3310;
+package main.java.uta.cse3310;
 
 public class Games {
 
-    PlayerType Players;
+    PlayerType Players;             //referenced from <<PlayerType>> enum
+    GameType GameType;              //referenced from <<GameType>> enum
     public PlayerType[] Button;
     public int GameId;
+
+    Game() {
+        //initializes Players, Button, and GameType
+        //sets up the game visible to players
+    }
+
+    public void StartGame() {
+        //timer starts
+        //chat starts
+    }
+
+    public int PlayerToIdx(PlayerType P) {
+        //NOTE: this method may not be necessary since PlayerType enums
+        //are already indexed in this case (not X or O)
+        int idx = 0;
+        return idx;
+    }
+
+    public void Update(UserEvent U) {
+        //reacts to user input
+        //no turns, real time reaction for letter selections
+        //points update when word is won
+    }
+
+    public void tick() {
+        //timer function
+    }
+
+    public bool wordCheck() {
+        //validates a match was made by player
+    }
 
 }
---
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -0,0 +1,9 @@
+//Kaitlin Martin
+//Group 6
+
+package main.java.uta.cse3310;
+
+public class Leaderboard {
+    Vector<Player> GamesWonPlace = new Vector<>();
+    Vector<Player> PointsEarnedPlace = new Vector<>();
+}

518cf44e50aa6a9b6187bcc030339e434669f3c7
Laura Luu <lml9874@mavs.uta.edu>
2024-03-17 21:44:16 -0500

    messageBoard.java wordBank.java

--- src/main/java/uta/cse3310/wordBank.java
+++ src/main/java/uta/cse3310/wordBank.java
@@ -1,16 +1,32 @@
 package uta.cse3310
 
 import java.util.*
-
+import java.util.Vector;
 public class wordBank
 {
+   Vector<Word> WordList = new Vector<>();
+   Vector<Word> foundWords = new Vector<>();
    public wordBank()
    {
    
    }
-   public void generateWords()
+   public void add(Word)
    {
 
    }
 
+   public Word getWordList()
+   {
+      return null;
+   }
+   
+   public Word getFoundWords()
+   {
+      return null;
+   }
+   
+   public void generateWords(Vector<Word>)
+   {
+      
+   }
 }

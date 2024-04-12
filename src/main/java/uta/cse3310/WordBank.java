3881407b6670436f163ebc685c50f53069208d53
Rudy Orozco <rjo4902@mavs.uta.edu>
2024-04-12 12:24:49 -0500

    Changes, still need to complete full implementation

--- src/main/java/uta/cse3310/WordBank.java
+++ src/main/java/uta/cse3310/WordBank.java
@@ -42,12 +42,12 @@ public class WordBank
 
    public Vector<Word> getWordList()
    {
-      return WordList;  // returns the word list
+      return WordList;                 // returns the word list
    }
    
    public Vector<Word> getFoundWords()
    {
-      return foundWords;   // returns the foundwords list
+      return foundWords;               // returns the foundwords list
    }
    
    public void generateWords(Vector<Word> wordList)
@@ -55,18 +55,29 @@ public class WordBank
       Random rng = new Random();
 
       // Generate 20 words from the total list
-      for(int i = 0; i < 20; i++) {
-         int rand = rng.nextInt(numwords);
+      for(int i = 0; i < 20; i++) {                            // For each word
+         int rand = rng.nextInt(numwords);                     // create a random word 
 
          for(int j = 0; j < elements.size(); j++) {
-            if(elements.contains(rand))
+            if(elements.contains(rand))                        // if an element already exists
             {
-               rand = rng.nextInt(numwords);
-               j = 0;
+               rand = rng.nextInt(numwords);                   // run the random generator again
+               j = 0;                                          // set the increment back to 0 and run the for loop again
             }
          }
+         
+         WordType type = WordType.horizontal;   
+         int[] xcoord = new int[50];                           // NOTE: Probably need to change the type of parameters from an int[] to int
+         int[] ycoord = new int[50];                           // As there is no need to really have an array and rather just a value of those coordinates instead.
 
-         wordList.add(new Word(0, 0, totalList.get(rand)));
+         wordList.add(new Word(xcoord, ycoord, type, totalList.get(rand)));   
       }
    }
 }
+
+/*
+ * things to be done for generator:
+ * - Create a random coordinate assignment 
+ * - coorisponding type assignment
+ * - optimizations to the code.
+ */

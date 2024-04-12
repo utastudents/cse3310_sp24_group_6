6441e92e0771fc565b14f3abab8587de20c195cd
Rudy Orozco <rjo4902@mavs.uta.edu>
2024-04-12 00:37:04 -0500

    Still working on WordBank

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -15,24 +15,31 @@ public class Player {
 
     // Constructor
     public Player(String PlayNick, int PlayerNum) {
-        /*this.PlayNick = PlayNick;
+        this.PlayNick = PlayNick;
         this.PlayerNum = PlayerNum;
         this.Wins = 0;
         this.Points = 0;
         this.SavedPin = 0;
         this.HighScore = 0;
-        this.Coord1 = new int[0]; // Assuming default size
-        this.Coord2 = new int[0];*/ 
+        //this.Coord1 = new int[MAX_GRID_SIZE-1]; // Assuming default size
+        //this.Coord2 = new int[MAX_GRID_SIZE-1]; 
     }
     
     public boolean checkUnique(Vector<Player> PlayerList) {
-        return true;
+        for(int i = 0; i < PlayerList.size(); i++) {            //  While going through the entire vector
+            if(this.PlayNick == PlayerList.get(i).PlayNick) {   //  If there is a copy of a name
+            }
+        }
+        return true;                                            //  Otherwise it is true                
     }
 
     // Method to save or update the SavedPin
     public void SavePin(int newPin) {
-        /*this.SavedPin = newPin;*/
+        this.SavedPin = newPin;                                 // Set the requested pin to the saved pin
     }
 
-    public void updateScore() {}
+    public int updateScore() {
+        // Question: Update Highschore or score in game?
+        return this.Points;
+    }
 }
--- src/main/java/uta/cse3310/WordBank.java
+++ src/main/java/uta/cse3310/WordBank.java
@@ -1,32 +1,72 @@
 package uta.cse3310;
 
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
 import java.util.*;
-import java.util.Vector;
+
 public class WordBank
 {
+   List<Integer> elements = new ArrayList<>();
+   Vector<String> totalList = new Vector<>();
    Vector<Word> WordList = new Vector<>();
    Vector<Word> foundWords = new Vector<>();
+   int numwords;
+
    public WordBank()
    {
+      // Checking the amount of lines in a file
+       // Amount of words 
+      try { 
+         File file = new File("words.txt");
+         Scanner scan = new Scanner(file);
+
+         while(scan.hasNextLine()) {
+            totalList.add(scan.nextLine());
+            numwords++;
+         }
+         scan.close();
+      }
+      catch (Exception e) {
+         e.getStackTrace();
+      }
+   } 
    
-   }
    public void add(Word newWord)
    {
-
+      this.foundWords.add(newWord);   // adds new word to the foundwords
    }
 
-   public Word getWordList()
+   public Vector<Word> getWordList()
    {
-      return null;
+      return WordList;  // returns the word list
    }
    
-   public Word getFoundWords()
+   public Vector<Word> getFoundWords()
    {
-      return null;
+      return foundWords;   // returns the foundwords list
    }
    
    public void generateWords(Vector<Word> wordList)
    {
-      
+      Random rng = new Random();
+
+      // Generate 20 words from the total list
+      for(int i = 0; i < 20; i++) {
+         int rand = rng.nextInt(numwords);
+
+         for(int j = 0; j < elements.size(); j++) {
+            if(elements.contains(rand))
+            {
+               rand = rng.nextInt(numwords);
+               j = 0;
+            }
+         }
+
+         wordList.add(new Word(0, 0, totalList.get(rand)));
+      }
    }
 }

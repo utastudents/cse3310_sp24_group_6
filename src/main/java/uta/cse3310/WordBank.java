08be4e315e063197f902d77ef30b4b6005f58c75
Diya <Diyaannshibu5@gmail.com>
2024-04-13 01:19:00 -0500

    Updated Word Bank

--- src/main/java/uta/cse3310/WordBank.java
+++ src/main/java/uta/cse3310/WordBank.java
@@ -13,89 +13,118 @@ import java.util.Random;
 import java.lang.String;
 import java.util.*;
 
-public class WordBank
-{
-   private List<Integer> elements = new ArrayList<>();
-   private Vector<String> totalList = new Vector<>();
-   private Vector<Word> WordList = new Vector<>();
-   private Vector<Word> foundWords = new Vector<>();
-   private int numwords;
+public class WordBank {
+    private Vector<String> totalList = new Vector<>();
+    private Vector<Word> WordList = new Vector<>();
+    private Vector<Word> foundWords = new Vector<>();
+    private List<Integer> elements = new ArrayList<>();
+    private Random rng = new Random();
 
-   public WordBank()
-   {
-      // Checking the amount of lines in a file
-       // Amount of words 
-      try { 
-         File file = new File("words.txt");
-         Scanner scan = new Scanner(file);
-
-         while(scan.hasNextLine()) {
-            String s = scan.nextLine();
-            if(s.length() > 4) {
-               totalList.add(s);
-               numwords++;
+    public WordBank() {
+        try {
+            File file = new File("words.txt");
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()) {
+                String s = scan.nextLine();
+                if(s.length() > 4) {  // Filtered to include the words greater than 4 letters
+                    totalList.add(s);
+                }
             }
-         }
-         scan.close();
-      }
-      catch (Exception e) {
-         e.getStackTrace();
-      }
-   } 
-   
-   public void add(Word newWord)
-   {
-      this.foundWords.add(newWord);   // adds new word to the foundwords
-   }
+            scan.close();
+        } catch (Exception e) {
+            e.printStackTrace();  // Will print the stack trace if there's an exception
+        }
+    }
 
-   public Vector<Word> getWordList()
-   {
-      return this.WordList;                 // returns the word list
-   }
-   
-   public Vector<Word> getFoundWords()
-   {
-      return this.foundWords;               // returns the foundwords list
-   }
+    public void add(Word newWord) {
+        foundWords.add(newWord);  // Adds new word to the found words
+    }
 
-   public void printWordList(Vector<Word> wordList) {
-      for(Word w: wordList) {
-         System.out.println(w);
-      }
-      System.out.println("");
-   }
+    public Vector<Word> getWordList() {
+        return WordList;  // Returns the word list
+    }
 
-   public void wordsort(Vector<Word> wordList) {
-      Collections.sort(wordList, new Comparator<Word>() {
-         @Override
-         public int compare(Word first, Word sec) {
-            return first.getWord().compareTo(sec.getWord());
-         }
-      });
-   }
-   
-   public void generateWords()
-   {
-      Random rng = new Random();
+    public Vector<Word> getFoundWords() {
+        return foundWords;  // Returns the found words list
+    }
 
-      // Generate 100 words from the total list
-      for(int i = 0; i < 100; i++) {                            // For each word
-         int rand = rng.nextInt(numwords);                     // create a random word 
+    public void printWordList() {
+        for(Word w : WordList) {
+            System.out.println(w);
+        }
+    }
 
-         for(int j = 0; j < this.elements.size(); j++) {
-            if(this.elements.contains(rand))                        // if an element already exists
-            {
-               rand = rng.nextInt(numwords);                   // run the random generator again
-               j = 0;                                          // set the increment back to 0 and run the for loop again
+    public void generateWords() {
+        Collections.shuffle(totalList);  // Shuffled to ensure random selection
+        elements.clear();  // Clear previous indices to start fresh
+
+        // Ensures that no more than 100 words or the total number of words, whichever is smaller
+        int wordsToGenerate = Math.min(100, totalList.size());
+
+        for (int i = 0; i < wordsToGenerate; i++) {
+            int index = rng.nextInt(totalList.size());
+
+            if (!elements.contains(index)) {
+                elements.add(index);  // Mark this index as used
+                String selectedWord = totalList.get(index);
+                Word newWord = new Word(selectedWord);
+
+                WordType type = WordType.values()[rng.nextInt(WordType.values().length)];
+                newWord.setWordType(type);
+
+                // Random starting coordinates within a presumed grid size
+                int startX = rng.nextInt(50);  // Assuming grid size of 10x10 for example
+                int startY = rng.nextInt(50);
+
+                // Calculate ending coordinates based on word type and length
+                int[] endCoords = calculateEndCoordinates(startX, startY, type, selectedWord.length());
+                if (isValidPosition(endCoords[0], endCoords[1])) {
+                    newWord.setCoord1(startX, startY);
+                    newWord.setCoord2(endCoords[0], endCoords[1]);
+                    WordList.add(newWord);
+                } else {
+                    // If the word doesn't fit, remove the index and decrement i to retry
+                    elements.remove(elements.size() - 1);
+                    i--;
+                }
+            } else {
+                //If the index was already used, retry the iteration
+                i--;
             }
-         }
+        }
+    }
 
-         this.WordList.add(new Word(this.totalList.get(rand)));   
-      }
+    private int[] calculateEndCoordinates(int startX, int startY, WordType type, int length) {
+        int endX = startX, endY = startY;
+        switch (type) {
+            case horizontal:
+            case bHorizontal:
+                endX += (type == WordType.horizontal ? length - 1 : -(length - 1));
+                break;
+            case vertical:
+            case bVertical:
+                endY += (type == WordType.vertical ? length - 1 : -(length - 1));
+                break;
+            case topLeftBottomRight:
+            case bottomRightTopLeft:
+                endX += (type == WordType.topLeftBottomRight ? length - 1 : -(length - 1));
+                endY += (type == WordType.topLeftBottomRight ? length - 1 : -(length - 1));
+                break;
+            case topRightBottomLeft:
+            case bottomLeftTopRight:
+                endX += (type == WordType.topRightBottomLeft ? -(length - 1) : length - 1);
+                endY += (type == WordType.topRightBottomLeft ? length - 1 : -(length - 1));
+                break;
+        }
+        return new int[]{endX, endY};
+    }
 
-      wordsort(this.WordList);
-   }
+    private boolean isValidPosition(int x, int y) {
+        // Assuming grid size is 50x50
+        return x >= 0 && x < 50 && y >= 0 && y < 50;
+    }
 }
+
 
 /*
  * things to be done for generator:

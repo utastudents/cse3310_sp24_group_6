35277db33997b2d94e2091be6c69c1959b116865
Diya <Diyaannshibu5@gmail.com>
2024-04-15 10:14:42 -0500

    changes in messageBoard

--- src/main/java/uta/cse3310/MessageBoard.java
+++ src/main/java/uta/cse3310/MessageBoard.java
@@ -2,22 +2,6 @@ package uta.cse3310;
 
 import java.util.*;
 
-/*public class MessageBoard
-{
-   String message;
-   
-   public MessageBoard()
-   {
-   
-   }
-   
-   public void generateMessage()
-   {
-
-   }   
-}*/
-
-
 public class MessageBoard {
     private Vector<String> messages = new Vector<>();
 
@@ -33,6 +17,13 @@ public class MessageBoard {
     // Generate a formatted message and add it to the messages
     public void generateMessage(String message) {
         messages.add("System: " + message);
+    }
+
+    // Simulate receiving input from a user interface
+    public void receiveInput(String playNick, String message) {
+        if (!message.trim().isEmpty()) {
+            addMessage(playNick, message);
+        }
     }
 
     // Get all the messages from the board

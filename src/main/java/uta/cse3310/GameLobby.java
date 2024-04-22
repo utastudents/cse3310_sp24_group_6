9b9b9368b5bdf01c2b9c94dae53811035b288a0b
kriscar1001 <kristin.carter@protonmail.com>
2024-04-22 10:49:41 -0500

    GameLobby redone to match the Game constructor

--- src/main/java/uta/cse3310/GameLobby.java
+++ src/main/java/uta/cse3310/GameLobby.java
@@ -3,30 +3,174 @@ import java.util.*;
 
 public class GameLobby 
 {
-    private int gameNum = 0;        // Number of players needed for game to start
-    private int playersInGame = 0;  // Number of players currently in a game
+    // private int gameNum = 0;        // Number of players needed for game to start
+    //private int playersInGame = 0;  // Number of players currently in a game
     private int gtWanted = 0;       // Game type the player is seeking
     private Game G = null;          // Game that will be returned 
-    private int matchFound = 0;
+    //private int matchFound = 0;
 
     private Vector<Player> LobbyList = new Vector<Player>();
     // PlayerList is all players that have logged into the game in this instance of the program running
     // LobbyList is all players who are waiting for their game to start, or who are currently in a game
 
-    public GameLobby() 
-    {
-    
-    }
+    public GameLobby() {}
 
-    // Passed in the current active games and the individual player seeking a game.
-    // Purpose is to match the player with an active game or to create a new game
-    // if none of the active games match the gametype the player is seeking.
-    // Max number of active games is 5 so if 5 are already running and the player wants
-    // a game of a type that does not match any of them, they will be asked to try 
-    // again later.
-    public Game matchMaking(Vector<Game> ActiveGames, Player player) 
+    // MATCHMAKING REDONE - Now only makes a new game when ther is enough players that want to play it
+    // instead of making a new game whenever a player requests a game type that has not already been requested
+    public Game matchMaking(Player player, Vector<Player> PlayerList) 
     {
-        gtWanted = player.PlayerNum;
+        int pForTwo = 0;
+        int pForThree = 0;
+        int pForFour = 0;
+
+        // Count up how many players currently want each type of game
+        for (Player player : PlayerList) 
+        {
+            gtWanted = player.PlayerNum;
+            // player will have a PlayerNum of 0 after they are put into a game 
+
+            if (gtWanted == 2)
+            {
+                ++pForTwo;
+            }
+            else if (gtWanted == 3)
+            {
+                ++pForThree;
+            }
+            else if (gtWanted == 4)
+            {
+                ++pForFour;
+            }
+        }
+
+        Player player1 = null;
+        Player player2 = null;
+        Player player3 = null;
+        Player player4 = null;
+        Player currPlayer = null;
+        int playersNeeded = 0;
+
+        // If there are enough players create and start the game 
+        if (pForTwo >= 2)
+        {
+            playersNeeded = 0;
+
+            // Get the first 2 players that want to join a 2 player game
+            while(playersNeeded < 2 )
+            {
+                for (int i = 0; i < PlayerList.size(); i++)
+                {
+                    currPlayer = PlayerList.get(i);
+                    gtWanted = currPlayer.PlayerNum;
+
+                    if (gtWanted == 2 && playersNeeded == 0)
+                    {
+                        player1 = currPlayer;
+                        ++playersNeeded;
+
+                    }
+                    else if (gtWanted == 2 && playersNeeded == 1)
+                    {
+                        player2 = currPlayer;
+                        ++playersNeeded;
+                    }
+                }
+            }
+
+            // Make new game
+            G = new Game(player1, player2);
+            
+            return G;
+        }
+        else if (pForThree >= 3)
+        {
+            playersNeeded = 0;
+
+            // Get the first 3 players that want to join a 3 player game
+            while(playersNeeded < 3 )
+            {
+                for (int i = 0; i < PlayerList.size(); i++)
+                {
+                    currPlayer = PlayerList.get(i);
+                    gtWanted = currPlayer.PlayerNum;
+
+                    if (gtWanted == 3 && playersNeeded == 0)
+                    {
+                        player1 = currPlayer;
+                        ++playersNeeded;
+
+                    }
+                    else if (gtWanted == 3 && playersNeeded == 2)
+                    {
+                        player2 = currPlayer;
+                        ++playersNeeded;
+                    }
+                    else if (gtWanted == 3 && playersNeeded == 3)
+                    {
+                        player3 = currPlayer;
+                        ++playersNeeded;
+                    }
+
+                }
+            }
+
+            // Make new game
+            G = new Game(player1, player2, player3);
+            
+            return G;
+        }
+        else if (pForFour >= 4)
+        {
+            playersNeeded = 0;
+
+            // Get the first 4 players that want to join a 4 player game
+            while(playersNeeded < 4 )
+            {
+                for (int i = 0; i < PlayerList.size(); i++)
+                {
+                    currPlayer = PlayerList.get(i);
+                    gtWanted = currPlayer.PlayerNum;
+
+                    if (gtWanted == 3 && playersNeeded == 0)
+                    {
+                        player1 = currPlayer;
+                        ++playersNeeded;
+                    }
+                    else if (gtWanted == 3 && playersNeeded == 2)
+                    {
+                        player2 = currPlayer;
+                        ++playersNeeded;
+                    }
+                    else if (gtWanted == 3 && playersNeeded == 3)
+                    {
+                        player3 = currPlayer;
+                        ++playersNeeded;
+                    }
+                    else if (gtWanted == 3 && playersNeeded == 3)
+                    {
+                        player4 = currPlayer;
+                        ++playersNeeded;
+                    }
+                }
+            }
+
+            // Make new game
+            G = new Game(player1, player2, player3, player4);
+    
+            return G;
+        }
+
+    }
+        
+        // OLD MATCH MAKING---------------------------------------------------------------------------------------------
+        // Passed in the current active games and the individual player seeking a game.
+        // Purpose is to match the player with an active game or to create a new game
+        // if none of the active games match the gametype the player is seeking.
+        // Max number of active games is 5 so if 5 are already running and the player wants
+        // a game of a type that does not match any of them, they will be asked to try 
+        // again later.
+
+        /*gtWanted = player.PlayerNum;
 
         // Look through the ActiveGames to see if a game matches what the player wants 
         for (int i = 0; i < ActiveGames.size(); i++)
@@ -92,7 +236,6 @@ public class GameLobby
             System.out.println("Sorry, maximum amount of games is in progress. Please try again later.");
         }
         
-        return G;
-    }
+        return G;*/
 
 }
